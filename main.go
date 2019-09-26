9761031bd4c5ebe5053bbe4870984c92b244beea
lwnmengjing <991154416@qq.com>
2019-09-26 17:08:09 +0800

    Update main.go

--- main.go
+++ main.go
@@ -20,11 +20,12 @@ func main() {
 	})
 
 	//log.Fatal(autotls.Run(r, "*.jifenhua.cn"))
-	m := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
+	//m := autocert.Manager{
+	//	Prompt:     autocert.AcceptTOS,
 		//HostPolicy: autocert.HostWhitelist("jifenhua.cn", "*.jifenhua.cn"),
 		//Cache:      autocert.DirCache("/data/www/.cache"),
-	}
+	//}
 
-	log.Fatal(autotls.RunWithManager(r, &m))
+	//log.Fatal(autotls.RunWithManager(r, &m))
+	r.Run(":80")
 }
